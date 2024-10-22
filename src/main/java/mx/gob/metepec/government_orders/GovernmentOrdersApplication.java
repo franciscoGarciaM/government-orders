125705652b63cfba00bfc628439c863fcda15c0f
Victor Manuel Ceron Tolentino <victor_ceronjpg@outlook.com>
2024-10-22 14:05:32 -0600

    First Commit

---
+++ src/main/java/mx/gob/metepec/government_orders/GovernmentOrdersApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernmentOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernmentOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/metepec/government_orders/GovernmentOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernmentOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
